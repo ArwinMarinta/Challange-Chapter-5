3bd715ef0d0c5273fe8748ae50f6cf9dd28f86cd
Arwin Marinta <arwinmarinta3@gmail.com>
2023-10-15 00:48:05 +0800

    update login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,19 +2,22 @@ import { useSearch } from "../contexts/SearchContext";
 import { BsSearch } from "react-icons/bs";
 import { axiosInstance } from "../lib/axios";
 import { useNavigate, Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Search from "../assets/search2.svg";
+import axios from "axios";
+import { token } from "../constants/config";
 
 const Navbar = () => {
   const navigate = useNavigate();
   const [openSearch, setOpenSearch] = useState(false);
+  const [user, setUser] = useState(null);
 
   const logout = (event) => {
     event.preventDefault();
 
     localStorage.removeItem("token");
 
-    window.location.replace("/");
+    window.location.replace("/login");
   };
 
   const {
@@ -49,56 +52,102 @@ const Navbar = () => {
     }
   };
 
+  //get me
+  const getMe = async () => {
+    try {
+      if (!token) return;
+      const response = await axiosInstance.get("/api/v1/auth/me");
+      const { data } = response.data;
+      setUser(data);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        alert(error?.response?.data?.message);
+        return;
+      }
+      alert(error?.message);
+    }
+  };
+
+  useEffect(() => {
+    getMe();
+  }, []);
+
   return (
     <div className="absolute left-0 right-0 top-0 z-40 w-full items-center bg-transparent">
       <nav className="mx-auto flex items-center justify-between  px-4 py-6 lg:px-10">
         <button onClick={handleClearSearch}>
-          <h1 className="text-lg font-extrabold text-red-600 md:text-6xl">
+          <h1 className="text-2xl font-extrabold text-red-600 md:text-6xl">
             MovieList
           </h1>
         </button>
-        <div
-          className="cursor-pointer lg:hidden"
-          onClick={() => setOpenSearch(openSearch ? false : true)}
-        >
-          <img src={Search} className="h-6 w-6" />
-        </div>
 
-        <div className="hidden lg:block">
-          <form
-            onSubmit={handleSubmitSearch}
-            className="relative flex w-80 items-center justify-center"
-          >
-            <input
-              placeholder="Seach any movies"
-              id="search_movie"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-              className="w-full rounded-full border-2 border-red-600 bg-transparent px-5 py-2 text-white outline-none backdrop-blur-md focus:border-red-800"
-            />
-            <button
-              type="submit"
-              className="absolute bottom-1/2 right-2 translate-y-1/2 rounded-full bg-slate-300 p-2 transition-colors"
+        {user && (
+          <>
+            <div
+              className=" cursor-pointer lg:hidden"
+              onClick={() => setOpenSearch(openSearch ? false : true)}
             >
-              <BsSearch className="h-5 w-5" />
-            </button>
-          </form>
-        </div>
+              <img src={Search} className="h-6 w-6" />
+            </div>
+
+            <div className="hidden lg:block">
+              <form
+                onSubmit={handleSubmitSearch}
+                className="relative flex w-[800px] items-center justify-center"
+              >
+                <input
+                  placeholder="Seach any movies"
+                  id="search_movie"
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                  className="w-full rounded-full border-2 border-red-600 bg-transparent px-5 py-2 text-white outline-none backdrop-blur-md focus:border-red-800"
+                />
+                <button
+                  type="submit"
+                  className="absolute bottom-1/2 right-2 translate-y-1/2 rounded-full bg-slate-300 p-2 transition-colors"
+                >
+                  <BsSearch className="h-5 w-5" />
+                </button>
+              </form>
+            </div>
+          </>
+        )}
         <div className="flex flex-row items-center justify-center gap-4">
-          <Link
-            className="rounded-lg border-2 border-red-700 px-3 py-2 font-bold text-red-700 hover:bg-white"
-            as={Link}
-            to="/login"
-          >
-            Login
-          </Link>
-          <Link
-            className="rounded-lg border-2 border-red-700 bg-red-700 px-3 py-2 font-bold text-white"
-            as={Link}
-            onClick={logout}
-          >
-            Register
-          </Link>
+          {user ? (
+            <>
+              <Link
+                className="rounded-lg border-2 border-red-700 px-3 py-2 font-bold text-red-700 hover:bg-white"
+                as={Link}
+                to="/login"
+              >
+                me
+              </Link>
+              <Link
+                className="rounded-lg border-2 border-red-700 bg-red-700 px-3 py-2 font-bold text-white"
+                as={Link}
+                onClick={logout}
+              >
+                Logout
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link
+                className="rounded-lg border-2 border-red-700 px-2 py-1 font-semibold text-red-700 hover:bg-white md:px-3 md:py-2 md:font-bold"
+                as={Link}
+                to="/login"
+              >
+                Login
+              </Link>
+              <Link
+                className="rounded-lg border-2 border-red-700 bg-red-700 px-2 py-1 font-semibold text-white md:px-3 md:py-2 md:font-bold"
+                as={Link}
+                to="/register"
+              >
+                Register
+              </Link>
+            </>
+          )}
         </div>
       </nav>
       <div className={`${openSearch ? "block" : "hidden"} lg:hidden`}>
--- src/pages/RegisterPage/index.jsx
+++ src/pages/RegisterPage/index.jsx
@@ -1,83 +1,91 @@
 // import React from 'react'
+import Navbar from "../../components/Navbar";
+import { Link } from "react-router-dom";
 
-const index = () => {
+const Register = () => {
   return (
-    <div className=" flex h-screen  items-center justify-center  ">
-      <div className="px-20  sm:w-[600px] md:border-2 ">
-        {/* Buat akun dan tombol leave */}
-        <div className="mb-6 mt-10 flex w-full flex-row justify-between">
-          <h1 className="text-2xl font-bold">Create Account </h1>
-          <button className="text-center text-2xl font-bold">X</button>
-        </div>
-
-        {/* Form inputan dan button register */}
-        <form className="flex flex-col gap-5 ">
-          <div>
-            <h2 className="mb-2 text-lg font-bold">Nama Depan</h2>
-            <input
-              className="h-10 w-full rounded-md border-2 pl-2 text-lg"
-              type="text"
-              placeholder="Nama Depan"
-            />
-          </div>
-          <div>
-            <h2 className="mb-2 text-lg font-bold">Nama Belakang</h2>
-            <input
-              className="h-10 w-full rounded-md border-2 pl-2 text-lg"
-              type="text"
-              placeholder="Nama Belakang"
-            />
-          </div>
-          <div>
-            <h2 className="mb-2 text-lg font-bold">Email</h2>
-            <input
-              className="h-10 w-full rounded-md border-2 pl-2 text-lg"
-              type="text"
-              placeholder="Email"
-            />
-          </div>
-          <div>
-            <h2 className="mb-2 text-lg font-bold">Password</h2>
-            <input
-              className="h-10 w-full rounded-md border-2 pl-2 text-lg"
-              type="text"
-              placeholder="Password"
-            />
-          </div>
-          <div>
-            <h2 className="mb-2 text-lg font-bold">Confirm Password</h2>
-            <input
-              className="h-10 w-full rounded-md border-2 pl-2 text-lg"
-              type="text"
-              placeholder="Confirm Password"
-            />
+    <>
+      <Navbar />
+      <div className=" flex h-screen  items-center justify-center">
+        <div className="px-20  py-8 sm:w-[600px] md:border-2">
+          {/* Buat akun dan tombol leave */}
+          <div className="mb-6  flex w-full flex-row justify-between">
+            <h1 className="text-2xl font-bold">Create Account </h1>
+            <Link
+              className="text-center text-2xl font-bold"
+              as={Link}
+              to="/login"
+            >
+              X
+            </Link>
           </div>
 
-          <button
-            type="submit"
-            className="rounded-xl border-2 bg-red-600  py-3 font-bold text-white "
-          >
-            Register Now
-          </button>
-        </form>
+          {/* Form inputan dan button register */}
+          <form className="flex flex-col gap-5 ">
+            <div>
+              <h2 className="mb-2 text-lg font-bold">Nama Depan</h2>
+              <input
+                className="h-10 w-full rounded-md border-2 pl-2 text-lg"
+                type="text"
+                placeholder="Nama Depan"
+              />
+            </div>
+            <div>
+              <h2 className="mb-2 text-lg font-bold">Nama Belakang</h2>
+              <input
+                className="h-10 w-full rounded-md border-2 pl-2 text-lg"
+                type="text"
+                placeholder="Nama Belakang"
+              />
+            </div>
+            <div>
+              <h2 className="mb-2 text-lg font-bold">Email</h2>
+              <input
+                className="h-10 w-full rounded-md border-2 pl-2 text-lg"
+                type="text"
+                placeholder="Email"
+              />
+            </div>
+            <div>
+              <h2 className="mb-2 text-lg font-bold">Password</h2>
+              <input
+                className="h-10 w-full rounded-md border-2 pl-2 text-lg"
+                type="text"
+                placeholder="Password"
+              />
+            </div>
+            <div>
+              <h2 className="mb-2 text-lg font-bold">Confirm Password</h2>
+              <input
+                className="h-10 w-full rounded-md border-2 pl-2 text-lg"
+                type="text"
+                placeholder="Confirm Password"
+              />
+            </div>
 
-        {/* Login dengan menggunakan google or facebook */}
-        <div className="mt-8 flex flex-col items-center">
-          <h1 className="text-base font-semibold">Or Sign Up Using</h1>
-          <div className="flex flex-row gap-3">
-            <button>google</button>
-            <button>Facebook</button>
+            <button
+              type="submit"
+              className="rounded-xl border-2 bg-red-600 py-2 font-bold text-white md:py-3 md:text-lg "
+            >
+              Register Now
+            </button>
+          </form>
+
+          {/* login ketika sudah memiliki akun  */}
+          <div className="mt-6 flex flex-col items-center gap-2 md:flex-row md:justify-center">
+            <p>Have already an account?</p>
+            <Link
+              className="font-bold hover:border-b-2 hover:border-black"
+              as={Link}
+              to="/login"
+            >
+              Login Here
+            </Link>
           </div>
-        </div>
-
-        {/* login ketika sudah memiliki akun  */}
-        <div className="mt-6 flex flex-col items-center md:flex-row md:justify-center">
-          <p>Have already an account?</p>
-          <p>Login Here</p>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
-export default index;
+export default Register;
